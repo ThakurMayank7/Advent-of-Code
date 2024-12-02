1060a06276c5ce86a1b7589b1f99ffa6c6ca5ada
Mayank <mayanksingh8181335335@gmail.com>
2024-12-02 17:19:04 +0530

    day 1 solved

---
+++ 2024/day1/solution.java
@@ -0,0 +1,36 @@
+class solution {
+    public static void main(String[] args) {
+
+        // object to access input data
+        Data data = new Data();
+
+        // input arrays
+        int c[][]=data.getParsedData();
+        int a[]=c[0];
+        int b[]=c[1];
+
+        // sorting both input array in ascending order
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                int temp = 0;
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+                if (b[j] > b[j + 1]) {
+                    temp = b[j];
+                    b[j] = b[j + 1];
+                    b[j + 1] = temp;
+                }
+            }
+        }
+
+        // calculating total distance
+        long distance = 0;
+        for (int i = 0; i < a.length; i++) {
+            distance += Math.abs(a[i] - b[i]);
+        }
+        System.out.println(distance);
+    }
+}
