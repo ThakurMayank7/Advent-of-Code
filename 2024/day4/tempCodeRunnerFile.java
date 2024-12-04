772371cfb83b495893344bf4cad2a981b887027f
Mayank <mayanksingh8181335335@gmail.com>
2024-12-05 00:32:05 +0530

    partially done day 3 problem1

---
+++ 2024/day4/tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+cking backwards
+                if(j>2 
+                && c.get(j)=='X' 
+                && c.get(j-1)=='M'
+                && c.get(j-2)=='A'
+                && c.get(j-3)=='S'
+                )
+                {
+                    // System.out.println("here"+data.get(i).size());
+                    counter++;
+                }
